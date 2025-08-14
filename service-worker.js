7603326d7e206c15bf5ed2ecb9745f837e21ee2a
alit2814 <puzzolanteluigi248@gmail.com>
2025-08-14 17:35:11 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,33 @@
+const CACHE_NAME = "canzoniere-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
 
